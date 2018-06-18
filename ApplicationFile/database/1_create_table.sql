5a02f04d770b4e44936c9dcfeeeec17f3bfa2a22
Quang <six3up@gmail.com>
2018-06-18 09:55:44 +0700

    create table role and user

---
+++ ApplicationFile/database/1_create_table.sql
@@ -0,0 +1,17 @@
+use toeiconline;
+
+create table user(
+	userid bigint not null primary key auto_increment,
+    name varchar(255),
+    password varchar(255),
+    fullname varchar(300),
+    createddate timestamp
+);
+
+create table user(
+	userid bigint not null primary key auto_increment,
+    name varchar(255),
+    password varchar(255),
+    fullname varchar(300),
+    createddate timestamp
+);
